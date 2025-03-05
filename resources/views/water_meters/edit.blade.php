72c8044b68a70cd8dc1ef373849a2889511bd328
sanjar <38586745+sanjar8855@users.noreply.github.com>
2025-03-06 00:05:33 +0500

    '.'

--- resources/views/water_meters/edit.blade.php
+++ resources/views/water_meters/edit.blade.php
@@ -89,7 +89,7 @@
                                     <!-- Download SVG icon from http://tabler.io/icons/icon/calendar -->
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" /><path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" /><path d="M11 15h1" /><path d="M12 15v3" /></svg>
                                 </span>
-                                <input name="expiration_date" class="form-control" placeholder="Sanani tanlang" id="datepicker-icon-prepend3"/>
+                                <input name="expiration_date" value="{{ $waterMeter->expiration_date }}" class="form-control" placeholder="Sanani tanlang" id="datepicker-icon-prepend3"/>
                             </div>
                             <script>
                                 document.addEventListener("DOMContentLoaded", function () {
@@ -121,7 +121,7 @@
                                     <!-- Download SVG icon from http://tabler.io/icons/icon/calendar -->
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" /><path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" /><path d="M11 15h1" /><path d="M12 15v3" /></svg>
                                 </span>
-                                <input {{ $waterMeter->last_reading_date }} name="last_reading_date" class="form-control" placeholder="Sanani tanlang" id="datepicker-icon-prepend2"/>
+                                <input value="{{ $waterMeter->last_reading_date }}" name="last_reading_date" class="form-control" placeholder="Sanani tanlang" id="datepicker-icon-prepend2"/>
                             </div>
                             <script>
                                 document.addEventListener("DOMContentLoaded", function () {
