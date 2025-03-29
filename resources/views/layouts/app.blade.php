9639248ea8cbc12384b6f0d8b68175b01499ffcd
sanjar <38586745+sanjar8855@users.noreply.github.com>
2025-03-29 22:34:13 +0500

    '.'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,10 +31,11 @@
                     aria-controls="sidebar-menu" aria-expanded="false" aria-label="Toggle navigation">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                     class="icon icon-tabler icons-tabler-outline icon-tabler-chart-pie-2">
+                     class="icon icon-tabler icons-tabler-outline icon-tabler-baseline-density-medium">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M12 3v9h9"/>
-                    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"/>
+                    <path d="M4 20h16"/>
+                    <path d="M4 12h16"/>
+                    <path d="M4 4h16"/>
                 </svg>
             </button>
             <div class="navbar-brand navbar-brand-autodark">
