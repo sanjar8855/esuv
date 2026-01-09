790a2a45009aa5e2278032c4d82e1d89ec1ed802
sanjar8855 <niggard98@mail.ru>
2026-01-09 14:29:28 +0500

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -454,6 +454,29 @@
                             </a>
                         </li>
                     @endcan
+                    @can('meter_readings')
+                        <li class="nav-item {{ request()->routeIs('mass_entry*') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('mass_entry.index') }}">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round"
+                                     class="icon icon-tabler icons-tabler-outline icon-tabler-list-check">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M3.5 5.5l1.5 1.5l2.5 -2.5"/>
+                                    <path d="M3.5 11.5l1.5 1.5l2.5 -2.5"/>
+                                    <path d="M3.5 17.5l1.5 1.5l2.5 -2.5"/>
+                                    <line x1="11" y1="6" x2="20" y2="6"/>
+                                    <line x1="11" y1="12" x2="20" y2="12"/>
+                                    <line x1="11" y1="18" x2="20" y2="18"/>
+                                </svg>
+                            </span>
+                                <span class="nav-link-title">
+                                Ommaviy kiritish
+                            </span>
+                            </a>
+                        </li>
+                    @endcan
                     @can('invoices')
                         <li class="nav-item {{ request()->routeIs('invoices*') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('invoices.index') }}">
